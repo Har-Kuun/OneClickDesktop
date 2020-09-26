18a0f0f5b4b83fdac773a01eb5555f3ba8e332b3
Har-Kuun <Har-Kuun@users.noreply.github.com>
2020-09-26 00:07:04 -0500

    Update OneClickDesktop_zh-CN.sh

--- OneClickDesktop_zh-CN.sh
+++ OneClickDesktop_zh-CN.sh
@@ -602,7 +602,7 @@ function main
 		fi
 		if [ $choice_rdpvnc = 1 ] ; then
 			echo 
-			say @B"请注意，使用上述用户名与密码登录Guacamole后，您还会需要在XRDP登录界面输入Linux系统用户名与密码(比如root用户，或者自行新建一个用户)" yellow
+			say @B"请注意，使用上述用户名与密码登录Guacamole后，您还会需要在XRDP登录界面输入Linux系统用户名与密码(比如root用户，或者自行新建一个用户)。Session选项请保持默认Xorg即可。" yellow
 		fi
 	fi
 	echo 
